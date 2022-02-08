fc89112b065e117bcf89fa5b2b3ab83a00232a21
MariaLuizaSantana <costamalu123@outlook.com>
2022-02-08 18:03:05 -0300

    projetocardvalidmarialuizasantana

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import validator from './validator.js';
  let botao1 = document.getElementById("botao")
     //ler o click do botao e executa a função que está no validador//
   botao1.addEventListener("click",function () {
+    
     let cardNumb = document.getElementById("r1").value
+
     let resultFinal = validator.isValid(cardNumb)
-console.log("click",cardNumb,resultFinal)
+
+    console.log("click",cardNumb,resultFinal)
   });
   
  console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,56 +1,51 @@
 const validator = {
   isValid:
+  function (logicaString){
+    console.log(logicaString, "string")
+    let cardNumb = logicaString.split("").reverse()
+    console.log(cardNumb,"array")
+    let primeiroEvent = 0
+    let segundoEvent = 0
+    let terceiroEvent = 0
 
-function (logicaString){
-  console.log(logicaString, "string")
-  let cardNumb = logicaString.split("").reverse()
-  console.log(cardNumb,"array")
-  let primeiroEvent = 0
-  let segundoEvent = 0
-  let terceiroEvent = 0
-
-  for (let i=0; i < cardNumb.length;i++) { 
-    let num = parseInt(cardNumb[i])
-    if ((i % 2 !== 0) && (num>=5)){
-      primeiroEvent = primeiroEvent + ((num*2)-9);
-      console.log(i, num, primeiroEvent,"par maior ou igual a 5")
+    for (let i=0; i < cardNumb.length;i++) { 
+      let num = parseInt(cardNumb[i])
+      if ((i % 2 !== 0) && (num>=5)){
+        primeiroEvent = primeiroEvent + ((num*2)-9);
+        console.log(i, num, primeiroEvent,"par maior ou igual a 5")
 
       }
       else if ((i % 2 !== 0) && (num<=5)) {
-    
+      
         segundoEvent = segundoEvent + (num *2);
         console.log(i,num,segundoEvent,"par menor ou igual a 5")
-    
-        }
-        else {
+      
+      }
+      else {
 
-          terceiroEvent = terceiroEvent + (num);
+        terceiroEvent = terceiroEvent + (num);
         console.log(terceiroEvent,"impar")
 
-       }
+      }
+    }
+    let sumFinal = 0
+      for (let i=0; i < cardNumb.length;i++) {
+        sumFinal = sumFinal + primeiroEvent + segundoEvent + terceiroEvent
+        console.log(sumFinal)
+        if (sumFinal % 10 === 0) {  
+          alert ("cartão válido")
+        }  
+        else {
+          alert ("cartão Inválido")
+        }
+      }
+      console.log(sumFinal)
+
+      if ((botao).value.length < 4){
+        alert ("Por favor preecha o número do cartão")
+      }
 
   }
-  let sumFinal = 0
-    for (let i=0; i < cardNumb.length;i++) {
-    sumFinal = sumFinal + primeiroEvent + segundoEvent + terceiroEvent
-    console.log(sumFinal)
-  if (sumFinal % 10 === 0) {  
-    alert ("cartão válido")
-   }  
-     else {
-       alert ("cartão Inválido")
-     }
-    }
-    console.log(sumFinal)
-
-    if ((botao).value.length < 4){
-      alert ("Por favor preecha o número do cartão")
-    }
-
 }
-
-
-}
-
-  export default validator;
+ export default validator;
 
